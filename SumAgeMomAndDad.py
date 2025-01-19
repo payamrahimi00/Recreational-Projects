e5d1c43e1e02aa2f7bdc7be90a0c5a4a2575ee57
پیام رحیمی <102457132+payamrahimi00@users.noreply.github.com>
2025-01-19 14:19:53 -0800

    Create SumAgeMomAndDad.py

---
+++ SumAgeMomAndDad.py
@@ -0,0 +1,15 @@
+"جمع کردن" به انگلیسی "addition" یا "summing" می‌شود.
+
+If you want to rename the function to reflect this, you can do so like this:
+
+```python
+
+
+def addition(Mom, Dad, Merge):
+    Mom = int(input("Enter your Mom's age: "))
+    Dad = int(input("Enter your Dad's age: "))
+    Merge = Mom + Dad
+    return Mom, Dad, Merge
+
+print(addition(0, 0, 0))
+```
