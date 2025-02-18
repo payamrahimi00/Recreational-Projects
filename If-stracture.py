3472f3eb96712a1b039042227af5c9d8fdbf0df0
Payamrahimi00 <Payamjoonn@gmail.com>
2025-02-18 12:25:52 +0330

    for(If)

---
+++ If-stracture.py
@@ -0,0 +1,13 @@
+
+age=int(input("insert Your Age:"))
+if age < 7:
+    Price=40
+elif age>10 and age<20:
+    Price=60
+elif age >25 and age <50:
+    Price=80
+else:
+    Price=100
+
+
+print(Price)
