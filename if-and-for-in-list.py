63910a6ede39f70d0439f1ea80a798ffddd5ddcf
Payamrahimi00 <Payamjoonn@gmail.com>
2025-02-18 13:56:23 +0330

    condition's in if

---
+++ if-and-for-in-list.py
@@ -0,0 +1,17 @@
+# cars=['Bmw','Mercedes','Audi','Pegout','Porche']
+# for car in cars:
+#     if car=='Bmw':
+#         print("Found!")
+# if 'Bmw' in cars:
+#     print("Found")
+car=[]
+
+# if len(car)==0:
+#     print("Empty")
+# else:
+#     print("Full")
+#The-car-true-or-false---the mean it's listed or not a listed.(list=true|false)
+# if car:
+#     print("ALright")
+# else:
+#     pass
