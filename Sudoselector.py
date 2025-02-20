2859da14cec40f2ed64eef2d213fea0824b70b13
Payamrahimi00 <Payamjoonn@gmail.com>
2025-02-20 16:18:00 +0330

    For sudo selector

---
+++ Sudoselector.py
@@ -0,0 +1,36 @@
+# Input_name=input("Insert yourname:")
+# i="*"
+# x=len(Input_name)
+# z=x*i
+# print(z)
+
+
+I='*'
+D=""
+B=0 
+while B<=5:
+    
+        if B==1:
+            print(D,D,D,D,D,I)
+        elif B==2:
+            print(D,D,D,D,I*3)
+        elif B==3:
+            print(D,D,D,I*5)
+        elif B==4:
+            print(D,D,I*8)
+        elif B==5:
+            print(D,I*10)
+        else:
+            pass
+        B+=1
+
+
+
+# x='3'*7
+# print(x)
+
+
+
+# row=7
+# for i in range(1, row+1):
+#     print('*'*i)
