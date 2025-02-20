464f027d0877bbac4c7f73a1b44efe75f7aa695d
Payamrahimi00 <Payamjoonn@gmail.com>
2025-02-20 14:08:55 +0330

    Amazing lopp in python

---
+++ While.py
@@ -0,0 +1,75 @@
+# Key=int(input("Insert Your number:"))
+# if Key>1000:
+#     exit()
+# else:
+#     pass
+# while Key !=1600:
+#     Key+=1
+#     print(Key)
+# user = input("Insert Your Text:")  # Prompts the user to input a text string
+# Num = input("Insert Your Target number:")  # Prompts the user to input a target number
+
+# like = True  # Initializes a boolean variable 'like' to True
+
+# # Checks if 'Num' is not an integer
+# if Num.isdigit():
+#     Num = int(Num)  # Converts 'Num' to an integer
+# else:
+#     like = False  # Sets 'like' to False if 'Num' is not a digit
+
+# # Checks if 'user' contains any digits
+# if any(char.isdigit() for char in user):
+#     like = False  # Sets 'like' to False if 'user' contains any digits
+
+# # While 'like' is True
+# while like:
+#     if Num <= 100:  # Checks if 'Num' is less than or equal to 100
+#         print(f"{Num} I love you {user}")  # Prints the current value of 'Num' and the user input text
+#         Num += 1  # Increments 'Num' by 1
+#     else:
+#         like = False  # Sets 'like' to False if 'Num' is greater than 100
+
+# Pid=int(input("Use of Number:"))
+# Txt=input("Use of txt:")
+# while Pid<=20:
+#     print(Pid*"\n",f"{Txt}")
+#     Pid+=1
+
+# Pid=int(input("Use of Number:"))
+# Txt=input("Use of txt:")
+# while Pid<=20:
+#     print(Pid*"\t",f"{Txt}")
+#     Pid+=1
+    
+
+# Pid=int(input("Use of Number:"))
+# Txt=input("Use of txt:")
+# while Pid<=20:
+#     print(Pid*"*",f"{Txt}")
+#     Pid+=1
+
+# Pid=int(input("Use of Number:"))
+# Txt=input("Use of txt:")
+# while Pid<=20:
+#     print(Pid*"*",f" {Pid}   {Txt}")
+#     Pid+=1
+
+# Pid = int(input("Use of Number:"))
+# Txt=['P','A','Y','A','M']
+# A=0
+# i="*"
+# while A<=5:
+    
+#     if A==1:
+#         print(A*i,Txt[0])
+#     elif A==2:
+#         print(A*i,Txt[1])
+#     elif A==3:
+#         print(A*i,Txt[2])
+#     elif A==4:
+#         print(A*i,Txt[3])
+#     elif A==5:
+#         print(A*i,Txt[4])
+#     else:
+#         pass
+#     A+=1
